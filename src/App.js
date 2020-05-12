04c0c3f31409c5b43e5d948a38ed9aad65f734ba
Lorenzo Luna <lorenzo.luna.def@hotmail.it>
2020-05-12 11:52:17 -0400

    Added features description from github

--- src/App.js
+++ src/App.js
@@ -74,18 +74,32 @@ class Info extends React.Component {
 
 class SidePanel extends React.Component {
     render() {
-        return (
-            <div className="col-content">
-                <h2>Game Rules</h2> 
-                The players take turns placing a token on the tiles. On the first turn the tiles are randomly shuffled and then the first player can only place a token on the tiles at the borders of the board. After the first turn a player can only place a token on a tile that shares a symbol or plant with the tile that the previously played token, which allows the players to influence and force each other's moves. The goal of the game is to achieve one of the three winning conditions.
-                <h3>Winning Conditions</h3>
-                <ul>
-                    <li>A player wins if he places four of his tokens in a line on the board. This includes vertical, horizontal, and diagonal lines.</li>
-                    <li>A player wins if he places four of his tokens in a square anywhere on the board.</li>
-                    <li>A player wins if after his move the other player cannot place his token anywhere.</li>
-                </ul>
-            </div>
-        )
+        const side = this.props.side;
+        if (side === "left"){
+            return (
+                <div className="col-content">
+                    <h2>Game Rules</h2> 
+                    The players take turns placing a token on the tiles. On the first turn the tiles are randomly shuffled and then the first player can only place a token on the tiles at the borders of the board. After the first turn a player can only place a token on a tile that shares a symbol or plant with the tile that the previously played token, which allows the players to influence and force each other's moves. The goal of the game is to achieve one of the three winning conditions.
+                    <h3>Winning Conditions</h3>
+                    <ul>
+                        <li>A player wins if he places four of his tokens in a line on the board. This includes vertical, horizontal, and diagonal lines.</li>
+                        <li>A player wins if he places four of his tokens in a square anywhere on the board.</li>
+                        <li>A player wins if after his move the other player cannot place his token anywhere.</li>
+                    </ul>
+                </div>
+            )
+        } else {
+            return (
+                <div className="col-content">
+                    <h2>Website Features</h2>
+                    <ul>
+                        <li>Clicking on a tile will place a token on it on behalf of the current player which is displayed at the bottom. You can only place a token on a tile that is shown in green, following the game's rules. You can play a full game by alternating between the players and placing tokens. The game ends when one of the winning conditions is achieved by one of the players, disallowing any further moves.</li>
+                        <li>At any moment you can restart the game by clicking on the New Game button. This will reset the state of the application and reshuffle the tiles on the board.</li>
+                        <li>The players' moves are saved and can be reviewed by using the Back and Forward buttons. You can use this feature to undo a move or replay the game with the same tile configuration. If you place a token when viewing a previous game state you will delete all the moves that have been made after it and will be able to continue the game from the move you have just made. This is great for trying different "what if" scenarios to see how your moves influence the outcome of the game.</li>
+                    </ul>    
+                </div>
+            )
+        }
     }
 }
 
@@ -256,7 +270,7 @@ render() {
     return (
         <div className="content">
             <div className="col left-col">
-                <SidePanel />
+                <SidePanel side={"left"}/>
             </div>
             <div className="game">
                 <div className="title">
@@ -268,7 +282,7 @@ render() {
                 <Info nextPlayer={nextPlayer} lastTile={lastTile} buttons={buttons} victory={previousPlayerVictory} vp={previousPlayer} />
             </div>
             <div className="col right-col">
-                <SidePanel />
+                <SidePanel side={"right"}/>
             </div>
         </div>
     );
